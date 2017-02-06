1251a5b961bedf814cebfd4f76bb4d7472a45f0d
K.Takata <kentkt@csc.jp>
2017-02-06 22:46:18 +0900

    Use 'travis encrypt-file' instead of 'travis encrypt'
    ```sh
    $ ssh-keygen -f ci@vimdoc-en.vim-jp.org -t rsa -b 4096 -C ci@vimdoc-en.vim-jp.org
    $ travis encrypt-file ci@vimdoc-en.vim-jp.org
    ```

--- tools/install_sshkey_github.sh
+++ tools/install_sshkey_github.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 #
-# USAGE: install_sshkey_github.sh {DECRYPT_KEY} {ENCRYPTED_SSHKEY_FILE} {DECRYPTED_SSHKEY_FILE}
+# USAGE: install_sshkey_github.sh {DECRYPT_KEY} {DECRYPT_IV} {ENCRYPTED_SSHKEY_FILE} {DECRYPTED_SSHKEY_FILE}
 
 set -e
 
 key=$1 ; shift
+iv=$1 ; shift
 src=$1 ; shift
 dst=$1 ; shift
 
 echo "Host github.com\n\tStrictHostKeyChecking no\n\tIdentityFile $dst\n" >> ~/.ssh/config
-openssl aes-256-cbc -k "$key" -in "$src" -d -a -out "$dst"
+openssl aes-256-cbc -K "$key" -iv "$iv" -in "$src" -d -out "$dst"
 chmod 600 "$dst"
