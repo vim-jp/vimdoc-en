8cbb09d4b3a1ec18745d5463a88aac04a1209731
K.Takata <kentkt@csc.jp>
2018-06-18 22:54:21 +0900

    Support HTTPS referrer

--- js/check-referrer.js
+++ js/check-referrer.js
@@ -1,16 +1,16 @@
 $(function() {
   'use strict';
   var REDIRECTS = {
-    'http://vim-jp.org/vimdoc-ja/ft_rust.html': true,
-    'http://vim-jp.org/vimdoc-ja/hebrew.html': true,
-    'http://vim-jp.org/vimdoc-ja/todo.html': true,
-    'http://vim-jp.org/vimdoc-ja/version5.html': true,
-    'http://vim-jp.org/vimdoc-ja/version6.html': true,
-    'http://vim-jp.org/vimdoc-ja/version7.html': true,
+    '//vim-jp.org/vimdoc-ja/ft_rust.html': true,
+    '//vim-jp.org/vimdoc-ja/hebrew.html': true,
+    '//vim-jp.org/vimdoc-ja/todo.html': true,
+    '//vim-jp.org/vimdoc-ja/version5.html': true,
+    '//vim-jp.org/vimdoc-ja/version6.html': true,
+    '//vim-jp.org/vimdoc-ja/version7.html': true,
   };
   var CLS = 'redirected-by-notranslation';
   var MSG = 'このページには日本語訳が存在しないために英語版に転送されました。';
-  if (REDIRECTS[document.referrer]) {
+  if (REDIRECTS[document.referrer.replace(/^https?:/, '')]) {
     $('.header').append('<div class="' + CLS + '">' + MSG + '</div>');
   }
 })
